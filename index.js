becffe752bb59dbc5ff38bbe86eaa91411468bbf
Paulo Daniel <paulo.araujo@uzzipay.com>
2021-11-05 00:01:37 -0300

    feat(update)

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const authToken = process.env.TWILIO_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 //Codigo trás informação do numero digitado
-client.lookups.v1.phoneNumbers('+5541987748768')
+client.lookups.v1.phoneNumbers('000000000000')
                  .fetch({type: ['carrier']})
                  .then(phone_number => console.log(phone_number.carrier));
 
 client.messages.create({
-    to: '+5569981297429',
+    to: '0000000000000',
     from: process.env.TWILIO_NUMBER,
     body: 'dudu zé ruela!',
 }).then(message => console.log(message)).catch(err => console.error(err));
